1ec407efdeeb9937c895c04ee94468ab3be07637
William Sinn <w.h.sinn@gmail.com>
2019-04-15 15:49:38 -0400

    first commit, add table relationships

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,9 @@
 require_relative '../config/environment'
 
+## One SWIMMER to many EVENTS
+## One EVENTS to many SWIMMERS
 
+## Many SWIMMERS to one swimmer_event
+## Many EVENTS to one swimmer_event
 
 puts "HELLO WORLD"
