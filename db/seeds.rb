6058f5c398c824c6971c019447d9478d4c6adf8e
William Sinn <w.h.sinn@gmail.com>
2019-04-16 10:35:26 -0400

    add seeds.rb, add seed data

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+robert = Swimmer.find_or_create_by(name: "robert", age: 16 , gender: 'M')
+darren = Swimmer.find_or_create_by(name: "darren", age: 16, gender: 'M')
+
+diana = Swimmer.find_or_create_by(name: "diana", age: 17 , gender: 'F')
+sophie = Swimmer.find_or_create_by(name: "sophie", age: 17 , gender: 'F')
+
+
+free200 = Event.find_or_create_by(name: "200 Freestyle", age: 17 , gender: 'F')
+butterfly100 = Event.find_or_create_by(name: "100 Butterfly", age: 16 , gender: 'M')
+
+Swim_event_time.find_or_create_by(swimmer: robert, event: free200, time: 115 )
+Swim_event_time.find_or_create_by(swimmer: darren, event: free200, time: 127 )
+
+Swim_event_time.find_or_create_by(swimmer: diana, event: butterfly100, time: 64 )
+Swim_event_time.find_or_create_by(swimmer: sophie, event: butterfly100, time: 70 )
